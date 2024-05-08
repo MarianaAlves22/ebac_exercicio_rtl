347f6b5ed4a768160dc33b95127100784646a8d9
MarianaAlves22 <127353284+MarianaAlves22@users.noreply.github.com>
2024-05-08 19:47:31 +0000

    fazendo correções no projeto

--- src/components/PostComments/__tests__/PostComments.tests.tsx
+++ src/components/PostComments/__tests__/PostComments.tests.tsx
@@ -1,36 +1,36 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from "@testing-library/react";
 
-import PostComments from "..";
+import PostComments from ".."; // Importo o componente
 
-describe("Teste para o componente PostComents", () => {
-    test("Deve adicionar dois comentarios corretamente", () => {
-        render(<PostComments />);
+describe("Teste para o componente PostComments", () => {
+  test("Deve adicionar dois comentários corretamente", () => {
+    render(<PostComments />);
 
-        const commentTextarea = screen.getByRole("textbox");
+    const commentTextarea = screen.getByRole("textbox");
 
-        fireEvent.change(commentTextarea, {
-            target: { value: "Primerio comentário" },
-        });
-
-        expect(commentTextarea).toHaveValue("Primeiro comentário");
-
-        const submitButton = screen.getByRole("button", { name: /comentar/i });
-
-        fireEvent.click(submitButton);
-
-        expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
-
-        fireEvent.change(commentTextarea, {
-            target: { value: "segundo comentário" },
-        });
-
-        expect(commentTextarea).toHaveValue("Segundo Comentario");
-
-        fireEvent.click(submitButton);
-
-        expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
-
-        expect(screen.getAllByTestId("post-comment")).toHaveLength(2);
+    fireEvent.change(commentTextarea, {
+      target: { value: "Primeiro comentário" },
     });
+
+    expect(commentTextarea).toHaveValue("Primeiro comentário");
+
+    const submitButton = screen.getByRole("button", { name: /comentar/i });
+
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+
+    fireEvent.change(commentTextarea, {
+      target: { value: "Segundo comentário" },
+    });
+
+    expect(commentTextarea).toHaveValue("Segundo comentário");
+
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
+    
+    expect(screen.getAllByTestId("post-comment")).toHaveLength(2);
+  });
 });
